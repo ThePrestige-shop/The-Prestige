667540af43f9c237000d7822f35b2400de3f4f44
Jay D <95277509+ThePrestige-shop@users.noreply.github.com>
2024-10-03 23:05:13 +0000

    New

--- Conserving Vultures/script.js
+++ Conserving Vultures/script.js
@@ -1,16 +1,1 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
-
-
-const x = document.getElementById("demo");
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude;
-}
