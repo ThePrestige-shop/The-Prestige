8bd90fc6c2ba2093aca85782ad131f69ca6be24e
Jay D <95277509+ThePrestige-shop@users.noreply.github.com>
2024-10-04 22:42:10 +0000

    New

--- Conserving Vultures/script.js
+++ Conserving Vultures/script.js
@@ -1,1 +1,18 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
+
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hidden1Elements = document.querySelectorAll('.hidden1');
+hidden1Elements.forEach((el) => observer.observe(el));
